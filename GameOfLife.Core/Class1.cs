6b833c472d7372a58eb32bf65f1966b17569aafb
peterprospl12 <piotrsulewski2003@gmail.com>
2025-11-03 21:57:27 +0100

    Add project files.

---
+++ GameOfLife.Core/Class1.cs
@@ -0,0 +1,8 @@
+
+namespace GameOfLife.Core
+{
+    public class Class1
+    {
+    }
+
+}
---
+++ GameOfLife.WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GameOfLife.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
